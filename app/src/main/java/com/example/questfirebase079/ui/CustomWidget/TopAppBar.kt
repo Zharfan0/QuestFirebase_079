fb42f7cb187cef9185c008a7a3afbb15e9ea22f9
ZHARFAN <zharfanwidyadana@gmail.com>
2025-01-15 13:12:37 +0700

    memperbaiki TopAppBar.kt

--- app/src/main/java/com/example/questfirebase079/ui/CustomWidget/TopAppBar.kt
+++ app/src/main/java/com/example/questfirebase079/ui/CustomWidget/TopAppBar.kt
@@ -15,28 +15,29 @@ import androidx.compose.ui.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CustomTopAppBar(
+fun TopAppBar(
     title: String,
     canNavigateBack: Boolean,
     modifier: Modifier = Modifier,
     scrollBehavior: TopAppBarScrollBehavior? = null,
     navigateUp: () -> Unit = {},
     onRefresh: () -> Unit = {}
-){
+) {
     CenterAlignedTopAppBar(
-        title ={ Text(title) },
+        title = { Text(title) },
         actions = {
-            Icon(imageVector = Icons.Default.Refresh,contentDescription = null,modifier = Modifier.clickable {
-                onRefresh()
-            })
+            Icon(
+                imageVector = Icons.Default.Refresh,
+                contentDescription = "",
+                modifier = Modifier.clickable { onRefresh() }
+            )
         },
         modifier = modifier,
-        scrollBehavior = scrollBehavior, navigationIcon = {
-            if (canNavigateBack){
-                IconButton(
-                    onClick = navigateUp
-                ) {
-                    Icon(imageVector = Icons.Default.ArrowBack, contentDescription = null)
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null)
                 }
             }
         }
