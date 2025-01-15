7d66c61d81bc31957207b73d7084ce8ba9653c7f
ZHARFAN <zharfanwidyadana@gmail.com>
2025-01-15 13:21:11 +0700

    kode detail.kt dipindah ke detailViewModel.kt

--- app/src/main/java/com/example/questfirebase079/ui/ViewModel/detailViewModel.kt
+++ app/src/main/java/com/example/questfirebase079/ui/ViewModel/detailViewModel.kt
@@ -1,2 +1,34 @@
 package com.example.questfirebase079.ui.ViewModel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questfirebase079.model.Mahasiswa
+import com.example.questfirebase079.repository.MahasiswaRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+
+sealed class DetailUiState {
+    object Loading : DetailUiState()
+    data class Success(val mahasiswa: Mahasiswa) : DetailUiState()
+    data class Error(val message: String) : DetailUiState()
+}
+
+class DetailViewModel(private val repository: MahasiswaRepository) : ViewModel() {
+    private val _uiState = MutableStateFlow<DetailUiState>(DetailUiState.Loading)
+    val uiState: StateFlow<DetailUiState> = _uiState
+
+    fun getMhs(nim: String) {
+        viewModelScope.launch {
+            _uiState.value = DetailUiState.Loading
+            repository.getMhs(nim)
+                .catch { exception ->
+                    _uiState.value = DetailUiState.Error("Error: ${exception.message}")
+                }
+                .collect { mahasiswa ->
+                    _uiState.value = DetailUiState.Success(mahasiswa)
+                }
+        }
+    }
+}
