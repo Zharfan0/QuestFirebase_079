a7d288cdf65fbe37547ecc9e9b94db3a50b1982c
ZHARFAN <zharfanwidyadana@gmail.com>
2025-01-15 13:12:13 +0700

    memperbaiki homeViewModel.kt

--- app/src/main/java/com/example/questfirebase079/ui/ViewModel/homeViewModel.kt
+++ app/src/main/java/com/example/questfirebase079/ui/ViewModel/homeViewModel.kt
@@ -11,49 +11,51 @@ import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 
-class HomeViewModel(private val mhs: MahasiswaRepository): ViewModel(){
-    var mhsUiState: HomeUiState by  mutableStateOf(HomeUiState.Loading)
+class HomeViewModel (
+    private val repoMhs: MahasiswaRepository
+) : ViewModel() {
+
+    var mhsUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
         private set
 
     init {
         getMhs()
     }
 
-
-    fun getMhs() {
+    fun getMhs(){
         viewModelScope.launch {
-            mhs.getMahasiswa()
-                .onStart {
-                    mhsUiState = HomeUiState.Loading
-                }
+            repoMhs.getAllMahasiswa().onStart {
+                mhsUiState = HomeUiState.Loading
+            }
                 .catch {
-                    mhsUiState = HomeUiState.Error(it)
+                    mhsUiState = HomeUiState.Error(e = it)
                 }
-                .collect{
-                    mhsUiState = if (it.isEmpty()){
-                        HomeUiState.Error(Exception("Belum ada data mahasiswa"))
-                    }else{
+                .collect {
+                    mhsUiState = if(it.isEmpty()) {
+                        HomeUiState.Error(Exception("Belum ada data Mahasiswa"))
+                    }else {
                         HomeUiState.Success(it)
+                        //it = data mahasiswa
                     }
                 }
         }
     }
 
-    fun deleteMhs(mahasiswa: Mahasiswa) {
+    fun deleteMhs(mahasiswa: Mahasiswa){
         viewModelScope.launch {
             try {
-                mhs.deleteMhs(mahasiswa)
-            } catch (e: Exception) {
-                mhsUIState = HomeUiState.Error(e)
+                repoMhs.deleteMhs(mahasiswa)
+            } catch (e: Exception){
+                mhsUiState = HomeUiState.Error(e)
             }
         }
     }
-
 }
 
-sealed class HomeUiState{
-    data class Success(val mahasiswa: List<Mahasiswa>): HomeUiState()
-    data class Error(val message: Throwable) : HomeUiState()
-    object Loading: HomeUiState()
 
+
+sealed class HomeUiState {
+    object Loading : HomeUiState()
+    data class Success(val data: List<Mahasiswa>) : HomeUiState()
+    data class Error(val e: Throwable) : HomeUiState()
 }
