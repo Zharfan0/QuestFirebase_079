90ac041d2c0c6abc7b50574d6190856f5995253c
ZHARFAN <zharfanwidyadana@gmail.com>
2025-01-15 12:48:02 +0700

    menambah atribut judulskripsi dan pembimbing pada Mahasiswa.kt

--- app/src/main/java/com/example/questfirebase079/model/Mahasiswa.kt
+++ app/src/main/java/com/example/questfirebase079/model/Mahasiswa.kt
@@ -1,12 +1,15 @@
 package com.example.questfirebase079.model
 
-data class Mahasiswa (
-    val nim: String,
-    val nama: String,
-    val jenis_kelamin: String,
-    val alamat: String,
-    val kelas: String,
-    val angkatan: String
+data class Mahasiswa(
+    val nim:String,
+    val nama:String,
+    val alamat:String,
+    val gender:String,
+    val kelas:String,
+    val angkatan:String,
+    val judulskripsi: String,
+    val pembimbing1: String,
+    val pembimbing2: String
 ){
-    constructor(): this("20220140079","Zharfan","Laki-laki","Jakarta","B","2022")
+    constructor() : this("", "", "", "", "", "","","","")
 }
